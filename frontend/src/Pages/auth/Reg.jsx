62d4e1621014a99ddb7b07c80e09ab77abbe9921
TechDev23 <pranaybbagat@gmail.com>
2023-08-09 12:52:28 +0530

    otp ui changes

--- frontend/src/Pages/auth/Reg.jsx
+++ frontend/src/Pages/auth/Reg.jsx
@@ -4,22 +4,23 @@ import {
   Checkbox,
   Button,
   Typography,
+  Spinner,
 } from "@material-tailwind/react";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import VerifiedIcon from '@mui/icons-material/Verified';
+import VerifiedIcon from "@mui/icons-material/Verified";
 
-import { onSignInSubmit, verifyCode} from "../../../utils/VerifyOtpHelper"
+import { onSignInSubmit, verifyCode } from "../../../utils/VerifyOtpHelper";
 
 import { useEffect, useState } from "react";
 import app from "../../firebase";
 import {
-    getAuth,
-    RecaptchaVerifier,
-    signInWithPhoneNumber,
-  } from "firebase/auth";
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
 import { toast } from "react-toastify";
 
 import { useRegisterUserMutation } from "../../redux/api/authApi";
@@ -28,13 +29,14 @@ import { Link, useNavigate } from "react-router-dom";
 const Reg = () => {
   const [showOtpField, setShowOtpField] = useState(false);
   const [mobileVerified, setMobileVerified] = useState(false);
-  const [ verifyBtnMsg, setVerifyBtnMsg] = useState("Verify");
+  const [verifyBtnMsg, setVerifyBtnMsg] = useState("Verify");
   const [otpField, setOtpField] = useState("");
-
+  const [sendingOtp, setSendingOtp] = useState(false);
+  const [verifyingOtp, setVerifyingOtp] = useState(false);
   const [registerUser, { isLoading, isError, error, isSuccess }] =
     useRegisterUserMutation();
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // validation using zod
   const phoneRegex = new RegExp(
@@ -54,11 +56,11 @@ const Reg = () => {
         .min(1, "Password is required")
         .min(8, "Password must have more than 8 characters"),
       confirmPassword: z.string().min(1, "Password confirmation is required"),
-    //   terms: z.literal(true, {
-    //     errorMap: () => ({
-    //       message: "You must accept the terms and conditions",
-    //     }),
-    //   }),
+      //   terms: z.literal(true, {
+      //     errorMap: () => ({
+      //       message: "You must accept the terms and conditions",
+      //     }),
+      //   }),
     })
     .refine((data) => data.password === data.confirmPassword, {
       path: ["confirmPassword"],
@@ -73,75 +75,90 @@ const Reg = () => {
     formState: { errors },
   } = useForm({ resolver: zodResolver(formSchema) });
 
-  
-
   const onVerifyClick = () => {
-    const phone_no = getValues("phone_no")
+    const phone_no = getValues("phone_no");
     setShowOtpField(true);
   };
 
   // ? Mobile verification started
   const auth = getAuth(app);
-  const onCaptchaVerify=() =>{
-    window.recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha-container", {
-      size: "invisible",
-      callback: (response) => {
-        // reCAPTCHA solved, allow signInWithPhoneNumber.
-      },
-    }, auth);
-  }
+  const onCaptchaVerify = () => {
+    if (!window.recaptchaVerifier) {
+      window.recaptchaVerifier = new RecaptchaVerifier(
+        auth,
+        "recaptcha-container",
+        {
+          size: "invisible",
+          callback: (response) => {
+            // reCAPTCHA solved, allow signInWithPhoneNumber.
+          },
+        },
+        auth
+      );
+    }
+    window.recaptchaVerifier.render();
+  };
 
-  const onSignInSubmit = ()=> { // user input
-    setShowOtpField(true);
-    onCaptchaVerify()
+  const onSignInSubmit = () => {
+    // user input
+    setSendingOtp(true);
+    onCaptchaVerify();
     const appVerifier = window.recaptchaVerifier;
-    const phoneNum ="+91"+ getValues("phone_no")
+    const phoneNum = "+91" + getValues("phone_no");
     signInWithPhoneNumber(auth, phoneNum, appVerifier)
       .then((confirmationResult) => {
         // SMS sent. Prompt user to type the code from the message, then sign the
         // user in with confirmationResult.confirm(code).
         window.confirmationResult = confirmationResult;
+        toast.success("OTP sent");
         setVerifyBtnMsg("otp sent");
+        setSendingOtp(false);
+        setShowOtpField(true);
       })
       .catch((error) => {
+        setSendingOtp(false);
         console.log(`SMS not sent ${error}`);
       });
-  }
+  };
 
-  const verifyCode=()=> {
+  const verifyCode = () => {
     const code = otpField;
-    if(code.length === 6 ){
-      confirmationResult
-      .confirm(code)
-      .then((result) => {
-        // User signed in successfully.
-        const user = result.user;
-        console.log("user from verify otp code", user)
-        toast.success("Mobile number verified")
-        setShowOtpField(false);
-        setMobileVerified(true);
-        setVerifyBtnMsg("")
-      })
-      .catch((error) => {
-        // User couldn't sign in (bad verification code?)
-        toast.error('Invalid otp', error)
-      });
+    setVerifyingOtp(true);
+    if(code.length < 6 || code.length > 6){
+      toast.error("Enter valid otp");
     }
-    
-  }
+    if (code.length === 6) {
+      confirmationResult
+        .confirm(code)
+        .then((result) => {
+          // User signed in successfully.
+          const user = result.user;
+          console.log("user from verify otp code", user);
+          toast.success("Mobile number verified");
+          setShowOtpField(false);
+          setMobileVerified(true);
+          setVerifyingOtp(false);
+          setVerifyBtnMsg("");
+        })
+        .catch((error) => {
+          // User couldn't sign in (bad verification code?)
+          toast.error("Invalid otp", error);
+          setVerifyingOtp(false);
+        });
+    }
+  };
   //  Mobile verification ended
 
   //Starting register api
   const regUsr = async (data) => {
-    const { confirmPassword, ...formData } = data; 
-    // console.log(formData)
+    const { confirmPassword, ...formData } = data;
     try {
-        const regUser = await registerUser(formData).unwrap()
-        toast.success(regUser.message)
-        navigate('/user/login')
+      const regUser = await registerUser(formData).unwrap();
+      toast.success(regUser.message);
+      navigate("/user/login");
     } catch (error) {
-        // toast.error(error);
-        toast.error(error?.data.detail)
+      // toast.error(error);
+      toast.error(error?.data.detail);
     }
   };
   //Ending register api
@@ -162,7 +179,7 @@ const Reg = () => {
         onSubmit={handleSubmit(regUsr)}
       >
         <div className="mb-4 flex flex-col gap-6">
-          <div className="flex gap-2 md:flex-row flex-col">
+          <div className="w-2/3 flex gap-2 md:flex-row flex-col">
             <div className="flex flex-col">
               {errors.first_name && (
                 <span className="text-red-600 text-xs pb-1 pl-1 w-full text-right">
@@ -170,7 +187,6 @@ const Reg = () => {
                 </span>
               )}
               <Input
-                size="lg"
                 label="First name"
                 color="amber"
                 {...register("first_name")}
@@ -190,54 +206,41 @@ const Reg = () => {
             </div>
           </div>
 
-          <div className="flex flex-col">
-            {errors.email_id && (
-              <span className="text-red-600 text-xs pb-1 pl-1 w-full text-right">
-                *{errors.email_id?.message}
-              </span>
-            )}
-            <Input
-              size="lg"
-              label="Email"
-              color="amber"
-              {...register("email_id")}
-            />
-          </div>
           {/* Phone number started */}
-          <div className="flex flex-col relative">
+          <div className="relative flex flex-col">
             {errors.phone_no && (
-              <span className="text-red-600 text-xs pb-1 pl-1 w-full text-right">
+              <span className="text-red-600 text-xs pb-1 pl-1 w-full text-left">
                 *{errors.phone_no?.message}
               </span>
             )}
+            <div className="relative">
             <Input
               size="lg"
-              className="tracking-widest"
+              className="!relative tracking-widest"
               label="Phone number"
               color="amber"
               disabled={mobileVerified}
+              icon={mobileVerified ? <VerifiedIcon fontSize="small"/>:<></>}
               {...register("phone_no")}
             />
             <Button
               size="sm"
               color="orange"
-              className="!absolute right-1 top-1.5 rounded hover:shadow-none"
+              className={`!absolute right-1 top-1.5 rounded hover:shadow-none ${mobileVerified ? "hidden" : '' }`}
               onClick={onSignInSubmit}
               disabled={mobileVerified}
             >
-              {verifyBtnMsg}
-              {
-                mobileVerified && <VerifiedIcon fontSize="small"/> 
-              }
+              {sendingOtp ? <Spinner color="amber" /> : verifyBtnMsg}
             </Button>
+            </div>
+            
             <div id="recaptcha-container"></div>
           </div>
-          {/* Phone number ended */}
 
-          {/* {showOtpField && ( */}
+          {showOtpField && (
             <>
               {/* ! OTP input starts */}
-              {/* <div className="flex flex-col">
+              <div className="flex flex-col">
                 {errors.email_id && (
                   <span className="text-red-600 text-xs pb-1 pl-1 w-full text-right">
                     *{errors.email_id?.message}
@@ -252,47 +255,66 @@ const Reg = () => {
                   onChange={(e) => setOtpField(e.target.value)}
                 />
                 <Button
-                  className="mt-6 hover:shadow-none"
+                  className="mt-6 hover:shadow-none flex items-center justify-center"
                   fullWidth
                   color="orange"
                   onClick={verifyCode}
                 >
-                  Verify OTP
+                  {verifyingOtp ? <Spinner color="amber"/>: "Verify"}
                 </Button>
-              </div> */}
+              </div>
             </>
-          {/* )} */}
+          )}
           {/* ! OTP input ends */}
-
-          <div className="flex flex-col">
-            {errors.password && (
-              <span className="text-red-600 text-xs pb-1 pl-1 w-full text-right">
-                * {errors.password?.message}
-              </span>
-            )}
-            <Input
-              type="password"
-              size="lg"
-              label="Password"
-              color="amber"
-              {...register("password")}
-            />
-          </div>
-          <div className="flex flex-col">
-            {errors.confirmPassword && (
-              <span className="text-red-600 text-xs pb-1 pl-1 w-full text-right">
-                *{errors.confirmPassword?.message}
-              </span>
-            )}
-            <Input
-              type="password"
-              size="lg"
-              label="Confirm password"
-              color="amber"
-              {...register("confirmPassword")}
-            />
-          </div>
+          {/* Phone number ended */}
         </div>
+
+        {mobileVerified ? (
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col">
+              {errors.email_id && (
+                <span className="text-red-600 text-xs pb-1 pl-1 w-full text-right">
+                  *{errors.email_id?.message}
+                </span>
+              )}
+              <Input
+                size="lg"
+                label="Email"
+                color="amber"
+                {...register("email_id")}
+              />
+            </div>
+            <div className="flex flex-col">
+              {errors.password && (
+                <span className="text-red-600 text-xs pb-1 pl-1 w-full text-right">
+                  * {errors.password?.message}
+                </span>
+              )}
+              <Input
+                type="password"
+                size="lg"
+                label="Password"
+                color="amber"
+                {...register("password")}
+              />
+            </div>
+            <div className="flex flex-col">
+              {errors.confirmPassword && (
+                <span className="text-red-600 text-xs pb-1 pl-1 w-full text-right">
+                  *{errors.confirmPassword?.message}
+                </span>
+              )}
+              <Input
+                type="password"
+                size="lg"
+                label="Confirm password"
+                color="amber"
+                {...register("confirmPassword")}
+              />
+            </div>
+          </div>
+        ):<></>}
+
         <Checkbox
           label={
             <Typography
@@ -305,13 +327,18 @@ const Reg = () => {
           }
           containerProps={{ className: "-ml-2.5" }}
         />
-        <Button type="submit" color="orange" className="mt-6 flex justify-center" fullWidth>
+        <Button
+          type="submit"
+          color="orange"
+          className="mt-6 flex justify-center"
+          fullWidth
+        >
           Register
         </Button>
         <Typography color="gray" className="mt-4 text-center font-normal">
           Already have an account?{" "}
           <Link
-            to={'/user/login'}
+            to={"/user/login"}
             className="font-medium text-orange-500 transition-colors hover:text-orange-700"
           >
             Sign In
