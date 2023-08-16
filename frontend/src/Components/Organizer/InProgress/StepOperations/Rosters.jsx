89d9d9b484c6d740d620633db983000dfb0cb672
TechDev23 <pranaybbagat@gmail.com>
2023-08-17 01:39:32 +0530

    roster

--- frontend/src/Components/Organizer/InProgress/StepOperations/Rosters.jsx
+++ frontend/src/Components/Organizer/InProgress/StepOperations/Rosters.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 //import { useDispatch, useSelector } from "react-redux";
@@ -115,13 +116,9 @@ function WinnerDialog({
 }
 
 function Match({ match }) {
-  
   const [team1Score, setTeam1Score] = useState("");
   const [team2Score, setTeam2Score] = useState("");
   const [scoreDialogOpen, setScoreDialogOpen] = useState(false);
-  
-
-  
 
   const handleOpenScoreDialog = () => {
     setScoreDialogOpen(true);
@@ -140,9 +137,7 @@ function Match({ match }) {
   };
 
   // handle submit score
-  const handlePostMatchScore = async () => {
-    
-  };
+  const handlePostMatchScore = async () => {};
   // handle post winner
   // const handlePostWinner = async ({ match_id }) => {
   //   console.log("Winner id", winnerId);
@@ -182,39 +177,31 @@ function Match({ match }) {
         team1Score={team1Score}
         team2Score={team2Score}
         handlePostMatchScore={handlePostMatchScore}
-        postScoreState={postScoreState}
+        // postScoreState={postScoreState}
       />
     </>
   );
 }
 
 export default function Rosters() {
- 
-
-  
   const [tourFixtures, setTourFixtures] = useState([]);
   const tourID = localStorage.getItem("createdTournamentID");
 
   useEffect(() => {
-    const fetchData = async () => {
-     
-    };
+    const fetchData = async () => {};
 
     fetchData();
   }, []);
 
-  const mappedMatches = tourFixtures?.reduce((matchesByRound, fixture) => {
-    
-  }, []);
+  const mappedMatches = tourFixtures?.reduce((matchesByRound, fixture) => {},
+  []);
 
-  const handleCreateFixtures = async () => {
-    
-  };
+  const handleCreateFixtures = async () => {};
 
   return (
     <div className="flex flex-col gap-2 min-h-screen">
       <div>
-        <Button
+        {/* <Button
           onClick={handleCreateFixtures}
           variant="outlined"
           color="orange"
@@ -225,9 +212,127 @@ export default function Rosters() {
             strokeWidth={2}
             className={`h-5 w-5`}
           />
-        </Button>
+        </Button> */}
+
+        <div className="flex flex-col w-full items-start justify-center gap-6">
+          <h1 className="font-semibold">Roster 1</h1>
+          <div className="flex flex-row w-full items-center justify-between gap-4">
+            <div className="w-72">
+              <Input label="Player 1" />
+            </div>
+
+            <Button color="orange">VS</Button>
+            <div className="w-72">
+              <Input label="Player 2" />
+            </div>
+            <textarea
+              className="flex-grow w-full h-10 border border-gray-500 px-4 py-2 rounded-lg focus:outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-200"
+              placeholder="Time Place and Responsible details"
+              rows={10}
+            ></textarea>
+          </div>
+
+          <div className="flex flex-row w-full items-center justify-between gap-4">
+            <div className="w-72">
+              <Input label="Player 3" />
+            </div>
+
+            <Button color="orange">VS</Button>
+            <div className="w-72">
+              <Input label="Player 4" />
+            </div>
+            <textarea
+              className="flex-grow w-full h-10 border border-gray-500 px-4 py-2 rounded-lg focus:outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-200"
+              placeholder="Time Place and Responsible details"
+              rows={10}
+            ></textarea>
+          </div>
+
+          <div className="flex flex-row w-full items-center justify-between gap-4">
+            <div className="w-72">
+              <Input label="Player 5" />
+            </div>
+
+            <Button color="orange">VS</Button>
+            <div className="w-72">
+              <Input label="Player 6" />
+            </div>
+            <textarea
+              className="flex-grow w-full h-10 border border-gray-500 px-4 py-2 rounded-lg focus:outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-200"
+              placeholder="Time Place and Responsible details"
+              rows={10}
+            ></textarea>
+          </div>
+
+          <div className="flex flex-row w-full items-center justify-between gap-4">
+            <div className="w-72">
+              <Input label="Player 7" />
+            </div>
+
+            <Button color="orange">VS</Button>
+            <div className="w-72">
+              <Input label="Player 8" />
+            </div>
+            <textarea
+              className="flex-grow w-full h-10 border border-gray-500 px-4 py-2 rounded-lg focus:outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-200"
+              placeholder="Time Place and Responsible details"
+              rows={10}
+            ></textarea>
+          </div>
+          {/* ================================ */}
+
+          <div className="flex flex-row w-full px-20 justify-between items-center">
+            <Button color="orange">Customize Roster</Button>
+            <Button color="orange">Declear Roster</Button>
+          </div>
+        </div>
+
+        <div className="my-10 border-t-2"></div>
+
+        <div className="flex flex-col w-full items-start justify-center gap-6">
+          <h1 className="font-semibold">Roster Round 2</h1>
+          <div className="flex flex-row w-full items-center justify-between gap-4">
+            <div className="w-72">
+              <Input label="Player 1" />
+            </div>
+
+            <Button color="orange">VS</Button>
+            <div className="w-72">
+              <Input label="Player 2" />
+            </div>
+            <textarea
+              className="flex-grow w-full h-10 border border-gray-500 px-4 py-2 rounded-lg focus:outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-200"
+              placeholder="Time Place and Responsible details"
+              rows={10}
+            ></textarea>
+          </div>
+
+          <div className="flex flex-row w-full items-center justify-between gap-4">
+            <div className="w-72">
+              <Input label="Player 3" />
+            </div>
+
+            <Button color="orange">VS</Button>
+            <div className="w-72">
+              <Input label="Player 4" />
+            </div>
+            <textarea
+              className="flex-grow w-full h-10 border border-gray-500 px-4 py-2 rounded-lg focus:outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-200"
+              placeholder="Time Place and Responsible details"
+              rows={10}
+            ></textarea>
+          </div>
+
+          {/* ================================ */}
+
+          <div className="flex flex-row w-full px-20 justify-between items-center">
+            <Button color="orange">Customize Roster</Button>
+            <Button color="orange">Declear Roster</Button>
+          </div>
+        </div>
       </div>
-      {mappedMatches.map((roundMatches, index) => (
+
+      {/* {mappedMatches.map((roundMatches, index) => (
         <div
           key={index}
           className="min-w-screen py-5 rounded border border-gray-600"
@@ -238,13 +343,13 @@ export default function Rosters() {
               <Match
                 key={match.id}
                 match={match}
-                postScoreState={postScoreState}
-                postResultState={postResultState}
+                // postScoreState={postScoreState}
+                // postResultState={postResultState}
               />
             ))}
           </div>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
