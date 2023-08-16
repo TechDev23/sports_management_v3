450dbdcdd84009a241c640a11350f35ede9090ea
Pranay Chavhan <pranay21chavhan@outlook.com>
2023-08-17 00:47:17 +0530

    Roster

--- frontend/src/Components/Organizer/Table.jsx
+++ frontend/src/Components/Organizer/Table.jsx
@@ -16,8 +16,8 @@ import {
   Progress,
 } from "@material-tailwind/react";
 
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
 import { BiCalendarAlt } from "react-icons/bi";
 import { MdAdminPanelSettings } from "react-icons/md";
 import { BsShieldCheck } from "react-icons/bs";
