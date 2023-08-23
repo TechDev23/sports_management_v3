c4bc8e66a704ac03b7afefda558593cd17c8101f
TechDev23 <pranaybbagat@gmail.com>
2023-08-23 20:10:34 +0530

    player dashboard

--- frontend/src/Pages/Explore/Features.jsx
+++ frontend/src/Pages/Explore/Features.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Button, Typography } from '@material-tailwind/react'
 
 const  FeaturesPage = () => {
   return (
-    <div> FeaturesPage</div>
+    <div className='w-full h-full p-5'>
+      <div className="relative h-96 w-full rounded-xl">
+        <img
+          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          alt="image 1"
+          className="h-full rounded-xl w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+            >
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80"
+            >
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that quality
+              of air that emanation from old trees, that so wonderfully changes
+              and renews a weary spirit.
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/User/Profile/Help.jsx
+++ frontend/src/Pages/User/Profile/Help.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Help = () => {
   return (
-    <div>Help</div>
+    <div className='w-full h-full'>
+        <div className='w-full flex justify-center items-center tracking-wider'>
+            <p className='text-4xl font-bold'>How can we help you</p>
+        </div>
+        <div>
+        
+        </div>
+    </div>
   )
 }
 
